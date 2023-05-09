b7790d6df95cad9aee4568f301bc951b9fb9a8d5
uyasuda <yasudayutaro123@gmail.com>
2023-05-09 17:28:52 +0900

    editButton

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,34 +1,60 @@
 import { TodoItem } from "../../types/TodoItemsTypes";
 import "./todo.css";
 import DELETE from "../../assets/delete.svg";
+import EDIT from "../../assets/edit.svg";
+import { useState } from "react";
 
 type todo = TodoItem & {
-  handleChange: any;
-  children: string | JSX.Element | JSX.Element[];
+    handleChange: any;
+    handleInputTextChange: any;
+    children: string | JSX.Element | JSX.Element[];
 };
 
-export function ToDo({ id, isCompleted, handleChange, text, children }: todo) {
-  return (
-    <li key={id}>
-      <input
-        className=""
-        type="checkbox"
-        checked={isCompleted}
-        onChange={handleChange}
-        id={id.toString()}
-      />
-      <span
-        style={{
-          textDecoration: isCompleted ? "line-through" : "none",
-          marginLeft: "5px",
-          marginRight: "5px",
-          maxWidth: "300px",
-          wordBreak: "break-all",
-        }}
-      >
-        <label htmlFor={id.toString()}>{text}</label>
-      </span>
-      {children}
-    </li>
-  );
+export function ToDo({
+    id,
+    isCompleted,
+    handleChange,
+    text,
+    children,
+    handleInputTextChange,
+}: todo) {
+    let [isEdit, setIsEdit] = useState(false);
+    const handleEditButton = () => {
+        setIsEdit(!isEdit);
+    };
+
+    return (
+        <li key={id}>
+            <input
+                className=""
+                type="checkbox"
+                checked={isCompleted}
+                onChange={handleChange}
+                id={id.toString()}
+            />
+            <span
+                style={{
+                    textDecoration: isCompleted ? "line-through" : "none",
+                    marginLeft: "5px",
+                    marginRight: "5px",
+                    maxWidth: "300px",
+                    wordBreak: "break-all",
+                }}
+            >
+                {isEdit ? (
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={handleInputTextChange}
+                    />
+                ) : (
+                    <label htmlFor={id.toString()}>{text}</label>
+                )}
+            </span>
+            <button id="delete">
+                <img src={EDIT} alt="delete" onClick={handleEditButton} />
+            </button>
+            {children}
+        </li>
+    );
 }
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -6,117 +6,131 @@ import { ToDo } from "../../components/ToDo/ToDo";
 import DELETE from "../../assets/delete.svg";
 
 export function Home() {
-  const [inputText, setInputText] = useState<string>("");
-  const [todoList, setTodoList] = useState<TodoItem[]>([]);
-  const [filterTypes, setFilterType] = useState<string>("all");
+    const [inputText, setInputText] = useState<string>("");
+    const [todoList, setTodoList] = useState<TodoItem[]>([]);
+    const [filterTypes, setFilterType] = useState<string>("all");
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputText(event.target.value);
-  };
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputText(event.target.value);
+    };
 
-  const handleAddButtonClick = () => {
-    if (inputText.trim() !== "") {
-      const newTodo: TodoItem = {
-        id: new Date().getTime(),
-        text: inputText,
-        isCompleted: false,
-      };
-      setTodoList([...todoList, newTodo]);
-      setInputText("");
-    } else {
-      alert("Please input your task name");
-    }
-  };
+    const handleAddButtonClick = () => {
+        if (inputText.trim() !== "") {
+            const newTodo: TodoItem = {
+                id: new Date().getTime(),
+                text: inputText,
+                isCompleted: false,
+            };
+            setTodoList([...todoList, newTodo]);
+            setInputText("");
+        } else {
+            alert("Please input your task name");
+        }
+    };
 
-  const handleCheckboxChange = (id: number) => {
-    const updateTodoList = todoList.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, isCompleted: !todo.isCompleted };
-      } else {
-        return todo;
-      }
-    });
-    setTodoList(updateTodoList);
-    console.log(todoList);
-  };
+    const handleCheckboxChange = (id: number) => {
+        const updateTodoList = todoList.map((todo) => {
+            if (todo.id === id) {
+                return { ...todo, isCompleted: !todo.isCompleted };
+            } else {
+                return todo;
+            }
+        });
+        setTodoList(updateTodoList);
+        console.log(todoList);
+    };
 
-  const handleFilter = (filter: string) => {
-    setFilterType(filter);
-  };
+    const handleFilter = (filter: string) => {
+        setFilterType(filter);
+    };
 
-  const handleDeleteButtonClick = (id: number) => {
-    const updatedTodoList = todoList.filter((todo) => todo.id !== id);
-    setTodoList(updatedTodoList);
-  };
+    const handleDeleteButtonClick = (id: number) => {
+        const updatedTodoList = todoList.filter((todo) => todo.id !== id);
+        setTodoList(updatedTodoList);
+    };
 
-  const filteredTodoList = () => {
-    if (filterTypes === "completed") {
-      return todoList.filter((todo) => todo.isCompleted);
-    } else if (filterTypes === "incomplete") {
-      return todoList.filter((todo) => !todo.isCompleted);
-    } else {
-      return todoList;
-    }
-  };
+    const filteredTodoList = () => {
+        if (filterTypes === "completed") {
+            return todoList.filter((todo) => todo.isCompleted);
+        } else if (filterTypes === "incomplete") {
+            return todoList.filter((todo) => !todo.isCompleted);
+        } else {
+            return todoList;
+        }
+    };
 
-  return (
-    <div className="container">
-      <div className="header">
-        <h1 className="title">My to-do list</h1>
-        <h2 className="subTitle">Lets do it!</h2>
-      </div>
-      <div className="form">
-        <input
-          id="taskInput"
-          placeholder="my to-do list"
-          type="text"
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <button className="submit-button" onClick={handleAddButtonClick}>
-          +
-        </button>
-      </div>
+    const handleInputTextChange = (event: any, id: number) => {
+        // const updatedTodoList = todoList.filter((todo) => todo.id === id)[0];
+        // updatedTodoList?.text = event.target.value;
+    };
 
-      <div
-        className="filter-container"
-        style={{ display: "flex", marginTop: "20px" }}
-      >
-        <button className="filter-button" onClick={() => handleFilter("all")}>
-          Dispaly All
-        </button>
-        <button
-          className="filter-button"
-          onClick={() => handleFilter("incomplete")}
-        >
-          Dispaly inComplete
-        </button>
-        <button
-          className="filter-button"
-          onClick={() => handleFilter("completed")}
-        >
-          Dispaly completed
-        </button>
-      </div>
-      <div className="todoListsContainer">
-        <ul>
-          {filteredTodoList().map((todo) => (
-            <ToDo
-              id={todo.id}
-              text={todo.text}
-              isCompleted={todo.isCompleted}
-              handleChange={() => handleCheckboxChange(todo.id)}
+    return (
+        <div className="container">
+            <div className="header">
+                <h1 className="title">My to-do list</h1>
+                <h2 className="subTitle">Lets do it!</h2>
+            </div>
+            <div className="form">
+                <input
+                    id="taskInput"
+                    placeholder="my to-do list"
+                    type="text"
+                    value={inputText}
+                    onChange={handleInputChange}
+                />
+                <button
+                    className="submit-button"
+                    onClick={handleAddButtonClick}
+                >
+                    +
+                </button>
+            </div>
+
+            <div
+                className="filter-container"
+                style={{ display: "flex", marginTop: "20px" }}
             >
-              <button
-                id="delete"
-                onClick={() => handleDeleteButtonClick(todo.id)}
-              >
-                <img src={DELETE} alt="delete" />
-              </button>
-            </ToDo>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+                <button
+                    className="filter-button"
+                    onClick={() => handleFilter("all")}
+                >
+                    Dispaly All
+                </button>
+                <button
+                    className="filter-button"
+                    onClick={() => handleFilter("incomplete")}
+                >
+                    Dispaly inComplete
+                </button>
+                <button
+                    className="filter-button"
+                    onClick={() => handleFilter("completed")}
+                >
+                    Dispaly completed
+                </button>
+            </div>
+            <div className="todoListsContainer">
+                <ul>
+                    {filteredTodoList().map((todo) => (
+                        <ToDo
+                            id={todo.id}
+                            text={todo.text}
+                            isCompleted={todo.isCompleted}
+                            handleChange={() => handleCheckboxChange(todo.id)}
+                            handleInputTextChange={(event: any) =>
+                                handleInputTextChange(event, todo.id)
+                            }
+                        >
+                            <button
+                                id="delete"
+                                onClick={() => handleDeleteButtonClick(todo.id)}
+                            >
+                                <img src={DELETE} alt="delete" />
+                            </button>
+                        </ToDo>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 }
