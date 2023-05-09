829ca4ae4d1f61ade25eef1e5726e7b2b2dcdc72
likai0626 <kai_li@reazon.jp>
2023-05-09 15:45:12 +0900

    15:45

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -5,15 +5,18 @@ import "./styles.css";
 type TodoItem = {
   id: number;
   text: string;
-  isCompleted: boolean;
+  isChecked: boolean;
 };
 
 export function Home() {
   const [inputText, setInputText] = useState<string>("");
   const [todoList, setTodoList] = useState<TodoItem[]>([]);
+  const [filterType,setFilterType] =  useState<"all"|"completed"|"uncompleted">("all")
+  const [inputError, setInputError] = useState<boolean>(false);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value);
+    setInputError(false);
   };
 
   const handleAddButtonClick = () => {
@@ -21,39 +24,123 @@ export function Home() {
       const newTodo: TodoItem = {
         id: new Date().getTime(),
         text: inputText,
-        isCompleted: false,
+        isChecked: false,
       };
       setTodoList([...todoList, newTodo]);
       setInputText("");
     }
-    console.log(todoList);
+    else {
+      alert("please input your mission!");
+    }
   };
 
-  return (
-    <div className="container">
-      <div className="header">
-        <h1 className="title">My to-do list</h1>
-        <h2 className="subTitle">Lets do it!</h2>
-      </div>
-      <div className="form">
-        <input
-          placeholder="my to-do list"
-          type="text"
-          value={inputText}
-          onChange={handleInputChange}
-        />
-        <button onClick={handleAddButtonClick}>+</button>
-      </div>
-      <div className="todoListsContainer">
-        <ul>
-          {todoList.map((todo) => (
-            <li key={todo.id}>
-              <input type="checkbox" checked={todo.isCompleted} />
-              {todo.text}
-            </li>
-          ))}
-        </ul>
-      </div>
+  const handleFilterTypeChange = (type: "all" | "completed" | "uncompleted") => {
+    setFilterType(type);
+  };
+
+const filteredTodoList = todoList.filter((todo) => {
+  if (filterType === "completed") {
+    return todo.isChecked;
+  } else if (filterType === "uncompleted") {
+    return !todo.isChecked;
+  } else {
+    return true;
+  }
+});
+
+return (
+  <div className="container">
+    <div className="header">
+      <h1 className="title">My to-do list</h1>
+      <h2 className="subTitle">Lets do it!</h2>
     </div>
-  );
+    <div className="form">
+      <input
+        placeholder="my to-do list"
+        type="text"
+        value={inputText}
+        onChange={handleInputChange}
+      />
+      <button onClick={handleAddButtonClick}>+</button>
+      {<div className="error">Please enter a task.</div>}
+    </div>
+    <div className="filterButtonsContainer">
+      <button
+        className={filterType === "all" ? "active" : ""}
+        onClick={() => handleFilterTypeChange("all")}
+      >
+        All
+      </button>
+      <button
+        className={filterType === "uncompleted" ? "active" : ""}
+        onClick={() => handleFilterTypeChange("uncompleted")}
+      >
+        Uncompleted
+      </button>
+      <button
+        className={filterType === "completed" ? "active" : ""}
+        onClick={() => handleFilterTypeChange("completed")}
+      >
+        Completed
+      </button>
+    </div>
+    <div className="todoListsContainer">
+      <ul>
+        {filteredTodoList.map((todo) => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.isChecked}
+                onChange={() => {
+                  const updatedTodoList = todoList.map((t) => {
+                    if (t.id === todo.id) {
+                      return { ...t, isChecked: !t.isChecked };
+                    } else {
+                      return t;
+                    }
+                  });
+                  setTodoList(updatedTodoList);
+                }}
+              />
+              <span className={todo.isChecked ? "completed" : ""}>
+                {todo.text}
+              </span>
+            </label>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
 }
+
+//   return (
+//     <div className="container">
+//       <div className="header">
+//         <h1 className="title">My to-do list</h1>
+//         <h2 className="subTitle">Lets do it!</h2>
+//       </div>
+//       <div className="form">
+//         <input
+//           placeholder="my to-do list"
+//           type="text"
+//           value={inputText}
+//           onChange={handleInputChange}
+//         />
+//         <button onClick={handleAddButtonClick}>+</button>
+//       </div>
+//       <div className="todoListsContainer">
+//         <ul>
+//           {todoList.map((todo) => (
+//             <li key={todo.id}>
+//               <input type="checkbox" checked={todo.isCompleted} />
+//               {todo.text}
+//             </li>
+//           ))}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
